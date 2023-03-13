19ab64559539be2b3d046ce89acc5d15d19f4470
DJordan131 <dylanjordan401@gmail.com>
2023-03-13 08:22:08 -0400

    Adding Weeks 8 and 9

--- W6/scriptCalc.js
+++ W6/scriptCalc.js
@@ -1,13 +1,13 @@
-
+    var yourfirstName = prompt("Please tell me your first name.", "Enter first name here.");
     var hourlyPay = prompt("Please enter your hourly pay", "Enter your hourly pay here:");  
     alert(`Your hourly pay is ${hourlyPay}`);
-
-    var hoursWorked = prompt("Please tell me how many hours you worked last week.", "Enter Hours worked here."); 
-    alert(`Hours worked is  ${hoursWorked}`);
+    var twoWeeks = prompt("How many hours do you work in 2 weeks?")
+    
+    alert(`Hours worked is  ${twoWeeks}`);
     var taxed = prompt("Enter the tax precentage", "Enter tax precentage here: ex. (20% = .2)"); 
     alert(`Your tax pay is ${taxed}`);
     
-    var gross = Number(hourlyPay) * Number(hoursWorked);
+    var gross = Number(hourlyPay) * Number(twoWeeks);
     var taxedAmount = Number(gross) * Number(taxed) 
     var netPay = Number(gross) - Number(taxedAmount)
     console.log(`Your gross pay is  ${gross}.\nYour Taxed amount is  ${taxedAmount} .\nYour net pay is ${netPay}.`);
